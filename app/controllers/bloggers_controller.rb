36d9ffa030709a6f8d0eb4b09f8b10a0acd940df
JingChen <jingchenjc2019@gmail.com>
2019-03-22 14:56:19 -0500

    Done.

--- app/controllers/bloggers_controller.rb
+++ app/controllers/bloggers_controller.rb
@@ -10,13 +10,7 @@ class BloggersController < ApplicationController
         @sum_likes = @likes.inject(0){| sum, x| sum + x }
         #get feature post
         @feature_post = @blogger.posts.max_by(&:likes)
-        #top 5 destinations this blogger has written on
-        @blogger.destinations.map { | destination | destination.posts.size}
 
-#         @blogger.posts.each do 
-# # destinations_ids =  @blogger.posts.map { |post| post.destination_id }
-# # freq_with_destination_ids = destinations_ids.uniq.map{|x| [x, destinations_ids.count(x)]}.to_h
-# # top_5_ids = freq_with_destination_ids.sort_by { |k,v| -v}.first(5).to_h.keys
     end
 
     def new
